9e8bc3f33ffecd29d9729ed74f35762ea31b7670
Asim Jawahir <45691479+CommanderAstern@users.noreply.github.com>
2023-03-14 02:24:05 +0530

    Added Chest Logic

--- Assets/Scripts/LookAtPlayer.cs
+++ Assets/Scripts/LookAtPlayer.cs
@@ -7,6 +7,9 @@ public class LookAtPlayer : MonoBehaviour
 void Update()
     {
         GameObject player = GameObject.FindGameObjectWithTag("MainCamera"); // or use the name of your player GameObject
+        // change y axis rotation by 180 degrees of player transofrm loot at
         transform.LookAt(player.transform);
+        transform.Rotate(0, 180, 0);
+        
     }
 }
