3778dc938fffc5588189863a369de92cf6d1f19d
Asim Jawahir <45691479+CommanderAstern@users.noreply.github.com>
2023-03-14 02:22:41 +0530

    minor solana changes

--- Solana/candyMachine/mintNFT.js
+++ Solana/candyMachine/mintNFT.js
@@ -1,7 +1,7 @@
 const { Metaplex, keypairIdentity, bundlrStorage, toMetaplexFile, toBigNumber, CreateCandyMachineInput, DefaultCandyGuardSettings, CandyMachineItem, toDateTime, sol, TransactionBuilder, CreateCandyMachineBuilderContext } =  require("@metaplex-foundation/js");
 const { Connection, clusterApiUrl, Keypair, PublicKey } =  require("@solana/web3.js");
 
-const COLLECTION_ADDRESS = "8EqVZxRstt3oVfxnEkQssDrCNFX5jNxYnMvp7Tt1b4dn";
+const COLLECTION_ADDRESS = "AwWLnHT316UZRKxQ4RFDbj1c529tRpj3xg7d1V563TGB";
 const WALLET = Keypair.fromSecretKey(new Uint8Array([142,22,131,43,13,8,195,223,58,97,14,198,114,96,93,224,217,8,214,194,129,36,100,8,60,174,1,110,153,235,139,203,0,120,34,158,223,14,131,160,82,6,217,95,10,175,140,163,218,168,41,24,140,226,3,150,87,166,176,140,159,27,196,34]));
 const WALLETV2 = Keypair.fromSecretKey(new Uint8Array([207,183,74,143,1,63,137,50,44,138,250,18,91,122,35,142,190,195,153,20,214,237,110,180,92,139,87,85,63,189,242,118,125,43,4,99,178,186,131,26,174,86,218,117,40,254,218,132,127,111,97,119,99,84,139,107,43,178,9,65,227,178,13,33]));
 const network = clusterApiUrl("devnet")
@@ -10,36 +10,36 @@ const METAPLEX = Metaplex.make(connection)
     .use(keypairIdentity(WALLET));
 const bs58 = require('bs58');
 
-async function checkNFT(candy_machine_address, owner_address) {
-    // const candyMachine = await METAPLEX
-    // .candyMachines()
-    // .findByAddress({ address: new PublicKey(candy_machine_address) }); 
-    // const myNfts = await METAPLEX.nfts().findAllByOwner({
-    //     owner: new PublicKey(owner_address)
-    // });
-    // var nfts = [];
-    // for (i in myNfts) {
-    //     if (myNfts[i] && myNfts[i].collection) {
-    //         if (myNfts[i].collection.address.toString() == COLLECTION_ADDRESS) {
-    //             let nft = await METAPLEX.nfts().load({ "metadata": myNfts[i] });
-    //             nfts.push(nft.json);
-    //         }
-    //       }
-    // }
-    // console.log(nfts);
+// async function checkNFT(candy_machine_address, owner_address) {
+//     // const candyMachine = await METAPLEX
+//     // .candyMachines()
+//     // .findByAddress({ address: new PublicKey(candy_machine_address) }); 
+//     // const myNfts = await METAPLEX.nfts().findAllByOwner({
+//     //     owner: new PublicKey(owner_address)
+//     // });
+//     // var nfts = [];
+//     // for (i in myNfts) {
+//     //     if (myNfts[i] && myNfts[i].collection) {
+//     //         if (myNfts[i].collection.address.toString() == COLLECTION_ADDRESS) {
+//     //             let nft = await METAPLEX.nfts().load({ "metadata": myNfts[i] });
+//     //             nfts.push(nft.json);
+//     //         }
+//     //       }
+//     // }
+//     // console.log(nfts);
 
-    const secretKeyString = 'n2upKhDxH3nQqwiy4sbDyxRKTC3WQ5cemJ8fg7iDgw7qGqPwhxJuQNthFQzLEibuymnpaDDfCk9zsRzWod2nR1k';
-    const secretKeyBytes = bs58.decode(secretKeyString);
-    const secretKeyUint8Array = new Uint8Array(secretKeyBytes);
-    let WALLETV3 = Keypair.fromSecretKey(secretKeyUint8Array);
-    console.log(WALLETV3.publicKey.toString());
-}
-checkNFT("LPyt8g7m96HN4XF7ShBKf5Z3Pp7PbaECZWwRDRDYcwt", "9Rc1PtEDtzAhXeGYJZJZxJBvF7YF85L7vDc2BTpsxNCQ");
+//     const secretKeyString = 'n2upKhDxH3nQqwiy4sbDyxRKTC3WQ5cemJ8fg7iDgw7qGqPwhxJuQNthFQzLEibuymnpaDDfCk9zsRzWod2nR1k';
+//     const secretKeyBytes = bs58.decode(secretKeyString);
+//     const secretKeyUint8Array = new Uint8Array(secretKeyBytes);
+//     let WALLETV3 = Keypair.fromSecretKey(secretKeyUint8Array);
+//     console.log(WALLETV3.publicKey.toString());
+// }
+// checkNFT("LPyt8g7m96HN4XF7ShBKf5Z3Pp7PbaECZWwRDRDYcwt", "9Rc1PtEDtzAhXeGYJZJZxJBvF7YF85L7vDc2BTpsxNCQ");
 
 async function mintNft() {
     const candyMachine = await METAPLEX
         .candyMachines()
-        .findByAddress({ address: new PublicKey("LPyt8g7m96HN4XF7ShBKf5Z3Pp7PbaECZWwRDRDYcwt") }); 
+        .findByAddress({ address: new PublicKey("3BXLUK6FZyQP4XD1AnutBtZ41Hnd1GtCFDfiZoPwaTE1") }); 
     let { nft, response } = await METAPLEX.candyMachines().mint(
         {
             candyMachine,
