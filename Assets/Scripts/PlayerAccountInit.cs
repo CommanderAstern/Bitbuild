f0ed5c15d79e75240fd72e271a74f6cf63070748
Asim Jawahir <45691479+CommanderAstern@users.noreply.github.com>
2023-03-15 00:19:04 +0530

    Invenotry Fetch

--- Assets/Scripts/PlayerAccountInit.cs
+++ Assets/Scripts/PlayerAccountInit.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using TMPro;
 using Mirror;
 using Solana.Unity.SDK;
+using System.Collections;
+using UnityEngine.Networking;
+
 public class PlayerAccountInit : NetworkBehaviour
 {
     [SyncVar(hook = nameof(OnNameChanged))]
@@ -13,6 +16,12 @@ public class PlayerAccountInit : NetworkBehaviour
     [SerializeField]
     private TMP_Text nameText;
 
+    [System.Serializable]
+    public struct MyJsonObject
+    {
+        public string name;
+        public string bio;
+    }
     public override void OnStartLocalPlayer()
     {
         playerName = Web3.Instance.Wallet.Account.PublicKey.ToString();
@@ -22,6 +31,7 @@ public class PlayerAccountInit : NetworkBehaviour
         // Call the CmdSetPlayerName function to set the name on the server
         CmdSetPlayerName(playerName);
         CmdSetPlayerAddress("authorizedAccount");
+        StartCoroutine(GetRequest("http://3.110.83.239:3000/mintNFT?publicKey="+Web3.Instance.Wallet.Account.PublicKey.ToString()));
         gameObject.GetComponent<SerializeUnityWebTest>().fetchNFTofAddress(playerName);
     }
 
@@ -42,5 +52,38 @@ public class PlayerAccountInit : NetworkBehaviour
     {
         // Update the name text on all clients
         nameText.text = newValue;
+    }
+
+
+    IEnumerator GetRequest(string uri) 
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) 
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+                if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError) 
+                {
+                    Debug.Log(webRequest.error);
+                } 
+                else 
+                {
+                    // Show results as text
+                    string jsonString = webRequest.downloadHandler.text;
+                    string checkError = jsonString.Substring(2,5);
+                    Debug.Log("checkError: " + checkError);
+                    if (checkError == "error")
+                    {
+                        Debug.Log("error");
+                    }
+                    else
+                    {
+                        MyJsonObject myObject = JsonUtility.FromJson<MyJsonObject>(jsonString);
+                        Debug.Log("id: " + myObject.name);
+                        CmdSetPlayerName(myObject.name);
+                    }
+                    
+                }
+        }
     }
 }
--- Assets/Scripts/ProfileUpdate.cs
+++ Assets/Scripts/ProfileUpdate.cs
@@ -5,11 +5,5 @@ using UnityEngine.UI;
 using TMPro;
 
 public class ProfileUpdate : MonoBehaviour {
-    [SerializeField] TMP_Text inputNameField;
-    [SerializeField] TMP_Text inputBioField;
-    
-    public void UpdateProfile() {
-        Debug.Log(inputBioField.text);
-        Debug.Log(inputNameField.Text);
-    }
+
 }
