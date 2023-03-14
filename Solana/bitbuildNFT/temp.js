329daac25a4ccf7af934d77476a258f97604ccf2
Asim Jawahir <45691479+CommanderAstern@users.noreply.github.com>
2023-03-15 04:01:32 +0530

    Modify Server IP

---
+++ Solana/bitbuildNFT/temp.js
@@ -0,0 +1,3 @@
+const web3 = require("@solana/web3.js");
+const bs58 = require('bs58');
+console.log(new Uint8Array(bs58.decode("2qUsk6oFK2nxyUwJi1N66ECnmGNg6Dwm2qF1HLYXqmP28hsXouGC1LxfbfewCDpmTrXWawKxHY7ybAwxCz6hSBba")));
